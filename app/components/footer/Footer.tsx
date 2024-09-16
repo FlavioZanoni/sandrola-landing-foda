4fd1ec1d4950555d705d449b968a582627636f61
FlavioZanoni <flaviozg888@gmail.com>
2024-09-15 22:57:39 -0300

    hm

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className=" w-full h-20 bg-neutral-800 ">
+      <ul className="h-full flex flex-row items-center pl-[20px]  gap-x-6 ">
+        <li>Flavin do zap</li>
+        <li>Images source:pexels.com</li>
+      </ul>
+    </div>
+  );
+}
+export default Footer;
